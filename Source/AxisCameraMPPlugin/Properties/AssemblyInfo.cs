ae6489fc8ed4fd6d12f4a30b6b5d36dc4232ff39
mattias.kindborg <mattias.kindborg@743037ad-d3f6-1e0c-2a57-c302d2592913>
2010-12-22 14:37:34 +0000

    Added main assembly skeleton.

---
+++ Source/AxisCameraMPPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Axis Camera MP Plugin")]
+[assembly: AssemblyDescription("A Axis camera plugin for the MediaPortal media center application.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("16cbf278-1df1-44ae-94f1-97b282718148")]
