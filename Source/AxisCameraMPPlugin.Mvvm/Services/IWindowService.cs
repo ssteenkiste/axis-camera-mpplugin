3aa760d6358b47ed86a6254e9bf655814cb2be80
mattias.kindborg <mattias.kindborg@743037ad-d3f6-1e0c-2a57-c302d2592913>
2010-12-23 00:32:10 +0000

    Added header to files.

--- Source/AxisCameraMPPlugin.Mvvm/Services/IWindowService.cs
+++ Source/AxisCameraMPPlugin.Mvvm/Services/IWindowService.cs
@@ -1,8 +1,27 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿#region Copyright (C) 2005-2010 Team MediaPortal
+
+// Copyright (C) 2005-2010 Team MediaPortal
+// http://www.team-mediaportal.com
+// 
+// MediaPortal is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MediaPortal is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MediaPortal. If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+using System.Diagnostics.CodeAnalysis;
 using System.Windows;
-using WinFormsDialogResult = System.Windows.Forms.DialogResult;
 using AxisCameraMPPlugin.Mvvm.Services.FrameworkDialogs.FolderBrowse;
 using AxisCameraMPPlugin.Mvvm.Services.FrameworkDialogs.OpenFile;
+using WinFormsDialogResult = System.Windows.Forms.DialogResult;
 
 namespace AxisCameraMPPlugin.Mvvm.Services
 {
