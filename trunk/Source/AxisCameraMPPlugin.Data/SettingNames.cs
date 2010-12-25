8277e504522f525c2ef752828af9767fba151a11
mattias.kindborg <mattias.kindborg@743037ad-d3f6-1e0c-2a57-c302d2592913>
2010-12-25 13:52:07 +0000

    Implemented plugin settings.

---
+++ trunk/Source/AxisCameraMPPlugin.Data/SettingNames.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AxisCameraMPPlugin.Data
+{
+	static class SettingNames
+	{
+		/// <summary>
+		/// The camera settings section name.
+		/// </summary>
+		internal static readonly string CameraSection = "camera";
+
+
+		/// <summary>
+		/// The cameras entry name.
+		/// </summary>
+		internal static readonly string CamerasEntry = "cameras";
+	}
+}
