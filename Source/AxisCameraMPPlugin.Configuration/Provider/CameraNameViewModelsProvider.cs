9ad3158babead9204dd4e93dd56accc5c932a29d
mattias.kindborg <mattias.kindborg@743037ad-d3f6-1e0c-2a57-c302d2592913>
2010-12-26 12:24:22 +0000

    Wrote stub methods for the command in SetupDialogViewModel, and implemented a provider for CameraNameViewModels.

---
+++ Source/AxisCameraMPPlugin.Configuration/Provider/CameraNameViewModelsProvider.cs
@@ -0,0 +1,46 @@
+﻿#region Copyright (C) 2005-2010 Team MediaPortal
+
+// Copyright (C) 2005-2010 Team MediaPortal
+// http://www.team-mediaportal.com
+// 
+// MediaPortal is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MediaPortal is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MediaPortal. If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+using System.Collections.Generic;
+using System.Linq;
+using AxisCameraMPPlugin.Configuration.ViewModel;
+using AxisCameraMPPlugin.Data;
+
+namespace AxisCameraMPPlugin.Configuration.Provider
+{
+	/// <summary>
+	/// Class describing a provider that provides a sequence of CameraNameViewModels.
+	/// </summary>
+	class CameraNameViewModelsProvider : ICameraNameViewModelsProvider
+	{
+		/// <summary>
+		/// Returns a collection of CameraNameViewModels.
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerable<CameraNameViewModel> Provide()
+		{
+			using (PluginSettings pluginSettings = new PluginSettings())
+			{
+				return pluginSettings
+					.GetCameras()
+					.Select(camera => new CameraNameViewModel(camera));
+			}
+		}
+	}
+}
--- Source/AxisCameraMPPlugin.Configuration/ViewModel/SetupDialogViewModel.cs
+++ Source/AxisCameraMPPlugin.Configuration/ViewModel/SetupDialogViewModel.cs
@@ -17,8 +17,10 @@
 // along with MediaPortal. If not, see <http://www.gnu.org/licenses/>.
 
 #endregion
+using System;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+using AxisCameraMPPlugin.Configuration.Provider;
 using AxisCameraMPPlugin.Mvvm;
 
 namespace AxisCameraMPPlugin.Configuration.ViewModel
@@ -28,6 +30,19 @@ namespace AxisCameraMPPlugin.Configuration.ViewModel
 	/// </summary>
 	class SetupDialogViewModel : DialogViewModelBase
 	{
+		public SetupDialogViewModel(ICameraNameViewModelsProvider cameraNameViewModelsProvider)
+		{
+			if (cameraNameViewModelsProvider == null) throw new ArgumentNullException("cameraNameViewModelsProvider");
+
+			Cameras = new ObservableCollection<CameraNameViewModel>(cameraNameViewModelsProvider.Provide());
+			
+			AddCommand = new RelayCommand(Add);
+			EditCommand = new RelayCommand(Edit, CanEdit);
+			RemoveCommand = new RelayCommand(Remove, CanRemove);
+		}
+
+
+
 		/// <summary>
 		/// Gets the cameras.
 		/// </summary>
@@ -65,6 +80,50 @@ namespace AxisCameraMPPlugin.Configuration.ViewModel
 		{
 			get { return Property(() => RemoveCommand); }
 			private set { Property(() => RemoveCommand, value); }
+		}
+
+
+		/// <summary>
+		/// Adds a new camera.
+		/// </summary>
+		private void Add(object parameter)
+		{
+		}
+
+
+		/// <summary>
+		/// Edits a camera.
+		/// </summary>
+		private void Edit(object parameter)
+		{
+		}
+
+
+		/// <summary>
+		/// Determines whether a camera can be edited.
+		/// </summary>
+		/// <returns>True if a camera can be edited; otherwise false.</returns>
+		private bool CanEdit(object parameter)
+		{
+			return false;
+		}
+
+
+		/// <summary>
+		/// Removes a camera.
+		/// </summary>
+		private void Remove(object parameter)
+		{
+		}
+
+
+		/// <summary>
+		/// Determines whether a camera can be removed.
+		/// </summary>
+		/// <returns>True if a camera can be removed; otherwise false.</returns>
+		private bool CanRemove(object parameter)
+		{
+			return false;
 		}
 	}
 }
