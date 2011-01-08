3f12871c7324012ad44dd62d226748e22d45867c
mattias.kindborg <mattias.kindborg@743037ad-d3f6-1e0c-2a57-c302d2592913>
2011-01-08 15:26:32 +0000

    Started implementing a progress dialog responsible for getting information about a camera.

---
+++ Source/AxisCameraMPPlugin.Configuration/ViewModel/ICameraCommunicationDialogViewModel.cs
@@ -0,0 +1,40 @@
+﻿#region Copyright (C) 2005-2010 Team MediaPortal
+
+// Copyright (C) 2005-2010 Team MediaPortal
+// http://www.team-mediaportal.com
+// 
+// MediaPortal is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MediaPortal is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MediaPortal. If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+using AxisCameraMPPlugin.Mvvm;
+
+namespace AxisCameraMPPlugin.Configuration.ViewModel
+{
+	/// <summary>
+	/// Interface for CameraCommunicationDialogViewModel.
+	/// </summary>
+	public interface ICameraCommunicationDialogViewModel : IDialogViewModelBase
+	{
+		/// <summary>
+		/// Gets the camera name.
+		/// </summary>
+		string Name { get; }
+
+
+		/// <summary>
+		/// Gets the snapshot path.
+		/// </summary>
+		string SnapshotPath { get; }
+	}
+}
