ff6469198d6d3044b38cc34def0af7141efb87f9
mattias.kindborg <mattias.kindborg@743037ad-d3f6-1e0c-2a57-c302d2592913>
2011-01-09 21:32:06 +0000

    Implemented a parameter parser.

---
+++ Source/AxisCameraMPPlugin.Configuration/Service/ParameterParser.cs
@@ -0,0 +1,60 @@
+﻿#region Copyright (C) 2005-2010 Team MediaPortal
+
+// Copyright (C) 2005-2010 Team MediaPortal
+// http://www.team-mediaportal.com
+// 
+// MediaPortal is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MediaPortal is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MediaPortal. If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+using System.Collections.Generic;
+
+namespace AxisCameraMPPlugin.Configuration.Service
+{
+	/// <summary>
+	/// Class responsible for parsing a camera parameter response.
+	/// </summary>
+	class ParameterParser : IParameterParser
+	{
+		/// <summary>
+		/// Parses the specified camera parameter response.
+		/// </summary>
+		/// <param name="parameters">The camera parameter response.</param>
+		/// <returns>A dictionary with parameter name as key, and parameter value as value.</returns>
+		public IDictionary<string, string> Parse(string parameters)
+		{
+			Dictionary<string, string> parsedParameters = new Dictionary<string, string>();
+
+			foreach (string parameter in parameters.Split('\n'))
+			{
+				string trimmedParameter = parameter.Trim();
+
+				int offset = trimmedParameter.IndexOf('=');
+				if (offset != -1)
+				{
+					string parameterName = trimmedParameter
+						.Substring(0, offset)
+						.Trim();
+
+					string parameterValue = trimmedParameter
+						.Substring(offset + 1, trimmedParameter.Length - offset - 1)
+						.Trim();
+
+					parsedParameters[parameterName] = parameterValue;
+				}
+			}
+
+			return parsedParameters;
+		}
+	}
+}
