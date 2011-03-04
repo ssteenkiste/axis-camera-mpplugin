6ff6ccc81e84e9e3441e8d3df603d4748f82122b
mattias.kindborg <mattias.kindborg@743037ad-d3f6-1e0c-2a57-c302d2592913>
2011-03-04 18:10:21 +0000

    Fixed issue 13. Added a project which is capable of starting the configuration dialogs.

--- Source/AxisCameras.Configuration.Starter/Properties/AssemblyInfo.cs
+++ Source/AxisCameras.Configuration.Starter/Properties/AssemblyInfo.cs
@@ -20,14 +20,13 @@
 using System;
 using System.Reflection;
 using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Axis Camera MP Plugin")]
-[assembly: AssemblyDescription("A Axis camera plugin for the MediaPortal media center application.")]
+[assembly: AssemblyTitle("Axis Cameras Configuration Starter")]
+[assembly: AssemblyDescription("Can start the Axis Cameras Configuration dialogs.")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -35,12 +34,5 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("16cbf278-1df1-44ae-94f1-97b282718148")]
-
-[assembly: CLSCompliant(false)]
+[assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
-
-// Provide access to internal types and methods to test functions and the Starter
-[assembly: InternalsVisibleTo("AxisCamerasTest")]
-[assembly: InternalsVisibleTo("AxisCameras.Configuration.Starter")]
