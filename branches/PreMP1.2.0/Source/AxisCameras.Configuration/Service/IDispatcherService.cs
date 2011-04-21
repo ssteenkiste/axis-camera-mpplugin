608e2d512d27471f110330f4679a821bfc2567dc
mattias.kindborg <mattias.kindborg@743037ad-d3f6-1e0c-2a57-c302d2592913>
2011-04-21 19:10:57 +0000

    Creating a branch for pre MediaPortal 1.2.0 development.

---
+++ branches/PreMP1.2.0/Source/AxisCameras.Configuration/Service/IDispatcherService.cs
@@ -0,0 +1,47 @@
+﻿#region Copyright (C) 2005-2010 Team MediaPortal
+
+// Copyright (C) 2005-2010 Team MediaPortal
+// http://www.team-mediaportal.com
+// 
+// MediaPortal is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MediaPortal is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MediaPortal. If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+using System;
+using System.Windows.Threading;
+
+namespace AxisCameras.Configuration.Service
+{
+	/// <summary>
+	/// Interface wrapping the Dispatcher.
+	/// </summary>
+	interface IDispatcherService
+	{
+		/// <summary>
+		/// Executes the specified delegate asynchronously with the specified arguments on the thread
+		/// that the Dispatcher was created on.
+		/// </summary>
+		/// <param name="method">
+		/// The delegate to a method that takes parameters specified in args, which is pushed onto the
+		/// Dispatcher event queue.
+		/// </param>
+		/// <param name="args">
+		/// An array of objects to pass as arguments to the given method. Can be null.
+		/// </param>
+		/// <returns>
+		/// An object, which is returned immediately after BeginInvoke is called, that can be used to
+		/// interact with the delegate as it is pending execution in the event queue.
+		/// </returns>
+		DispatcherOperation BeginInvoke(Delegate method, params object[] args);
+	}
+}
---
+++ branches/PreMP1.2.0/Source/AxisCameras.Configuration/ViewModel/ICameraParametersDialogViewModel.cs
@@ -0,0 +1,48 @@
+﻿#region Copyright (C) 2005-2010 Team MediaPortal
+
+// Copyright (C) 2005-2010 Team MediaPortal
+// http://www.team-mediaportal.com
+// 
+// MediaPortal is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MediaPortal is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MediaPortal. If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+using System;
+using AxisCameras.Mvvm;
+
+namespace AxisCameras.Configuration.ViewModel
+{
+	/// <summary>
+	/// Interface responsible for reading parameters from a camera.
+	/// </summary>
+	interface ICameraParametersDialogViewModel : IDialogViewModelBase, IDisposable
+	{
+		/// <summary>
+		/// Gets the friendly name.
+		/// </summary>
+		string FriendlyName { get; }
+
+
+		/// <summary>
+		/// Gets the firmware version.
+		/// </summary>
+		string FirmwareVersion { get; }
+
+
+		/// <summary>
+		/// Gets the video source count, i.e. the number of video source this camera (device actually)
+		/// has.
+		/// </summary>
+		int VideoSourceCount { get; }
+	}
+}
