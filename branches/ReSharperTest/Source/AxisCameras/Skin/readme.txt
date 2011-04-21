9c7cb82be3a772110c0bddf441cc45738d498b93
mattias.kindborg <mattias.kindborg@743037ad-d3f6-1e0c-2a57-c302d2592913>
2011-04-21 11:12:12 +0000

    Removed ReSharper test branch.

--- branches/ReSharperTest/Source/AxisCameras/Skin/readme.txt
+++
@@ -1,3 +0,0 @@
-﻿This namespace represents the skin folder in MediaPortal, i.e.
-
-  C:\ProgramData\Team MediaPortal\MediaPortal\skin
--- branches/ReSharperTest/Source/Setup/BuildSetup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-rem This bat file will build a setup
-
-"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /nologo /v:n /m /t:BuildSetup Build.proj
-
-pause
