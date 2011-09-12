390d96cb5afe0e969e96d838075874d198c73ea4
mattias.kindborg <mattias.kindborg@743037ad-d3f6-1e0c-2a57-c302d2592913>
2011-09-12 07:46:41 +0000

    Implemented a tool checking the plugin dependencies.

---
+++ trunk/Source/Tools/AxisCamerasDependencyChecker/AxisCamerasDependencyChecker/Program.cs
@@ -0,0 +1,26 @@
+﻿using AxisCamerasDependencyChecker.Dependencies;
+
+namespace AxisCamerasDependencyChecker
+{
+	internal class Program
+	{
+		private static readonly Printer Printer = new Printer();
+
+
+		/// <summary>
+		/// Entry method for console application.
+		/// </summary>
+		private static void Main()
+		{
+			// Check RTP source filter
+			IDependency axisRtpSourceFilter = new AxisRtpSourceFilter();
+			Printer.Print(axisRtpSourceFilter.Run());
+
+			// Check embedded source filter
+			IDependency embeddedAxisRtpSourceFilter = new EmbeddedAxisRtpSourceFilter();
+			Printer.Print(embeddedAxisRtpSourceFilter.Run());
+
+			Printer.Pause();
+		}
+	}
+}
