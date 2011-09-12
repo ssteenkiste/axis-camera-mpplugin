3e0f7c85f424e355ee7d2699a93e11f48b0dff8a
mattias.kindborg <mattias.kindborg@743037ad-d3f6-1e0c-2a57-c302d2592913>
2011-09-12 07:46:41 +0000

    Implemented a tool checking the plugin dependencies.

---
+++ Source/Tools/AxisCamerasDependencyChecker/AxisCamerasDependencyChecker/IDependency.cs
@@ -0,0 +1,14 @@
+﻿namespace AxisCamerasDependencyChecker
+{
+	/// <summary>
+	/// Interface describing a dependency.
+	/// </summary>
+	internal interface IDependency
+	{
+		/// <summary>
+		/// Runs verification of the dependency.
+		/// </summary>
+		/// <returns>The result of the dependency check.</returns>
+		DependencyResult Run();
+	}
+}
---
+++ Source/Tools/AxisCamerasDependencyChecker/AxisCamerasDependencyChecker/Printer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace AxisCamerasDependencyChecker
+{
+	/// <summary>
+	/// Class printing to <see cref="Console"/>.
+	/// </summary>
+	internal class Printer
+	{
+		private readonly ConsoleColor defaultForegroundColor;
+
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Printer"/> class.
+		/// </summary>
+		public Printer()
+		{
+			defaultForegroundColor = Console.ForegroundColor;
+		}
+
+
+		/// <summary>
+		/// Prints the outcome of the dependency result.
+		/// </summary>
+		internal void Print(DependencyResult result)
+		{
+			// Change foreground color
+			Console.ForegroundColor = result.IsSuccess ?
+				ConsoleColor.Green :
+				ConsoleColor.Red;
+
+			// Name
+			Console.WriteLine(
+				"{0} [{1}]",
+				result.Name,
+				result.IsSuccess ? "OK" : "FAILED");
+
+			// Reset foreground color
+			Console.ForegroundColor = defaultForegroundColor;
+
+			// Information
+			foreach (var information in result.Information)
+			{
+				Console.WriteLine("  " + information);
+			}
+
+			Console.WriteLine();
+		}
+
+
+		/// <summary>
+		/// Pauses the console, and lets the user continue by pressing any key.
+		/// </summary>
+		internal void Pause()
+		{
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
+		}
+	}
+}
