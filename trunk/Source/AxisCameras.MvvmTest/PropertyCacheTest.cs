d8510bf0c10b6eedd6f3e4173b41631614d94616
mattias.kindborg@gmail.com <mattias.kindborg@gmail.com@743037ad-d3f6-1e0c-2a57-c302d2592913>
2014-12-10 05:34:58 +0000

    Wrote unit test for PropertyCache.

--- trunk/Source/AxisCameras.MvvmTest/PropertyCacheTest.cs
+++ trunk/Source/AxisCameras.MvvmTest/PropertyCacheTest.cs
@@ -1,19 +1,68 @@
-﻿using NUnit.Framework;
+﻿using AxisCameras.Mvvm;
+using NUnit.Framework;
 
 namespace AxisCameras.MvvmTest
 {
     [TestFixture]
     public class PropertyCacheTest
     {
-        [Test]
-        public void AddMoreTests()
-        {
-            // ARRANGE
+        private PropertyCache propertyCache;
 
+        [SetUp]
+        public void SetUp()
+        {
+            propertyCache = new PropertyCache();
+        }
+
+        [Test]
+        public void GetUnknownValue()
+        {
             // ACT
+            var value = propertyCache.GetValue<int>("Unknown");
 
             // ASSERT
-            Assert.Fail();
+            Assert.That(value, Is.EqualTo(default(int)));
+        }
+
+        [Test]
+        public void GetKnownValue()
+        {
+            // ARRANGE
+            propertyCache.SetValue("Known", 42);
+
+            // ACT
+            var value = propertyCache.GetValue<int>("Known");
+
+            // ASSERT
+            Assert.That(value, Is.EqualTo(42));
+        }
+
+        [Test]
+        public void SetNewValue()
+        {
+            // ARRANGE
+            propertyCache.SetValue("Known", 1);
+
+            // ACT
+            bool success = propertyCache.SetValue("Known", 2);
+
+            // ASSERT
+            Assert.That(success, Is.True);
+            Assert.That(propertyCache.GetValue<int>("Known"), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void SetOldValue()
+        {
+            // ARRANGE
+            propertyCache.SetValue("Known", 1);
+
+            // ACT
+            bool success = propertyCache.SetValue("Known", 1);
+
+            // ASSERT
+            Assert.That(success, Is.False);
+            Assert.That(propertyCache.GetValue<int>("Known"), Is.EqualTo(1));
         }
     }
 }
