8c21876f25eaaba5e4dd40de00f14d0d58b33426
mattias.kindborg@gmail.com <mattias.kindborg@gmail.com@743037ad-d3f6-1e0c-2a57-c302d2592913>
2014-12-14 07:13:27 +0000

    Added tests for class RelayCommand.

--- trunk/Source/AxisCameras.Mvvm/RelayCommand.cs
+++ trunk/Source/AxisCameras.Mvvm/RelayCommand.cs
@@ -21,6 +21,7 @@
 using System;
 using System.Diagnostics;
 using System.Windows.Input;
+using AxisCameras.Core.Contracts;
 
 namespace AxisCameras.Mvvm
 {
@@ -35,59 +36,58 @@ namespace AxisCameras.Mvvm
     /// </summary>
     public class RelayCommand : ICommand
     {
-        #region Fields
-
-        private readonly Action<object> _execute;
-        private readonly Predicate<object> _canExecute;
-
-        #endregion // Fields
-
-        #region Constructors
-
-        /// <summary>
-        /// Creates a new command that can always execute.
-        /// </summary>
-        /// <param name="execute">The execution logic.</param>
-        public RelayCommand(Action<object> execute)
-            : this(execute, null)
-        {
-        }
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
 
         /// <summary>
         /// Creates a new command.
         /// </summary>
         /// <param name="execute">The execution logic.</param>
         /// <param name="canExecute">The execution status logic.</param>
-        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
         {
-            if (execute == null)
-                throw new ArgumentNullException("execute");
+            Requires.NotNull(execute);
 
-            _execute = execute;
-            _canExecute = canExecute;
+            this.execute = execute;
+            this.canExecute = canExecute;
         }
 
-        #endregion // Constructors
-
-        #region ICommand Members
-
-        [DebuggerStepThrough]
-        public bool CanExecute(object parameter)
-        {
-            return _canExecute == null ? true : _canExecute(parameter);
-        }
-
+        /// <summary>
+        /// Occurs when changes occur that affect whether or not the command should execute.
+        /// </summary>
         public event EventHandler CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
         }
 
-        public void Execute(object parameter)
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current
+        /// state.
+        /// </summary>
+        /// <param name="parameter">
+        /// Data used by the command.  If the command does not require data to be passed, this
+        /// object can be set to null.
+        /// </param>
+        /// <returns>
+        /// true if this command can be executed; otherwise, false.
+        /// </returns>
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
         {
-            _execute(parameter);
+            return canExecute == null || canExecute(parameter);
         }
 
-        #endregion // ICommand Members
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">
+        /// Data used by the command.  If the command does not require data to be passed, this
+        /// object can be set to null.
+        /// </param>
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
     }
 }
