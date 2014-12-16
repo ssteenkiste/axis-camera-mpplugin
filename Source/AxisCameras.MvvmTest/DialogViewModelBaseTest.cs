df160b2b9d43d5613851c8081cb0246029eca60e
mattias.kindborg@gmail.com <mattias.kindborg@gmail.com@743037ad-d3f6-1e0c-2a57-c302d2592913>
2014-12-16 20:41:08 +0000

    Added tests for class DialogViewModelBase.

---
+++ Source/AxisCameras.MvvmTest/DialogViewModelBaseTest.cs
@@ -0,0 +1,86 @@
+﻿using AxisCameras.Mvvm;
+using NUnit.Framework;
+
+namespace AxisCameras.MvvmTest
+{
+    [TestFixture]
+    public class DialogViewModelBaseTest
+    {
+        [Test]
+        public void DialogResultCommandIsNull()
+        {
+            // ARRANGE
+            var dialogViewModel = new DialogViewModel(true, true)
+            {
+                DialogResultCommand = null
+            };
+
+            // ACT
+            dialogViewModel.OkCommand.Execute(null);
+
+            // ASSERT
+            Assert.That(dialogViewModel.ValidateCalled, Is.True);
+            Assert.That(dialogViewModel.OnOkCalled, Is.True);
+        }
+
+        [TestCase(true, true, true, true, true)]
+        [TestCase(true, false, false, true, true)]
+        [TestCase(false, true, false, true, false)]
+        [TestCase(false, false, false, true, false)]
+        public void OkCommand(
+            bool isValid,
+            bool isOk,
+            bool expectedDialogResultCalled,
+            bool expectedValidateCalled,
+            bool expectedOnOkCalled)
+        {
+            // ARRANGE
+            bool dialogResultCommandCalled = false;
+
+            var dialogViewModel = new DialogViewModel(isOk, isValid)
+            {
+                DialogResultCommand = new RelayCommand(_ => dialogResultCommandCalled = true)
+            };
+
+            // ACT
+            dialogViewModel.OkCommand.Execute(null);
+
+            // ASSERT
+            Assert.That(dialogResultCommandCalled, Is.EqualTo(expectedDialogResultCalled));
+            Assert.That(dialogViewModel.ValidateCalled, Is.EqualTo(expectedValidateCalled));
+            Assert.That(dialogViewModel.OnOkCalled, Is.EqualTo(expectedOnOkCalled));
+        }
+
+        #region Helper classes
+
+        private class DialogViewModel : DialogViewModelBase
+        {
+            private readonly bool isOk;
+            private readonly bool isValid;
+
+            public DialogViewModel(bool isOk, bool isValid)
+            {
+                this.isOk = isOk;
+                this.isValid = isValid;
+            }
+
+            public bool OnOkCalled { get; private set; }
+
+            public bool ValidateCalled { get; private set; }
+
+            protected override bool OnOk()
+            {
+                OnOkCalled = true;
+                return isOk;
+            }
+
+            public override bool Validate()
+            {
+                ValidateCalled = true;
+                return isValid;
+            }
+        }
+
+        #endregion
+    }
+}
